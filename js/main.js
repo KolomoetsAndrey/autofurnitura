6d82d09fd8d0266401375eed55c38dc053093de0
Andrey Kolomoets <kolomoets.and@gmail.com>
2019-11-12 15:54:17 +0200

    title

--- js/main.js
+++ js/main.js
@@ -120,6 +120,8 @@ $(document).ready(function(){
         loop:true,
         margin:10,
         nav:false,
+        autoplay: true,
+        autoplayTimeout: 3000,
         responsive:{
             0:{
                 items:1
